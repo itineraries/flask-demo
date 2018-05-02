2cfa603157ad53460386e548fe134e8030b0b477
David Tsai <david@wecsam.com>
2018-05-02 12:54:18 -0400

    Added docstring to place_autocomplete __init__

--- place_autocomplete/__init__.py
+++ place_autocomplete/__init__.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+'''
+This module offers autocomplete suggestions for names of places. Call
+autocomplete to get a list of SourceSection objects. See types.py for more
+information on the SourceSection and Suggestion classes.
+
+Files are organized this way:
+ - source_*.py files contain a get_suggestions() function that takes the same
+   arguments as autocomplete() below
+ - pickle_*.py files will not be imported; they are scripts that prepare cache
+   files for their respective source_*.py files
+ - cache_*.pickle files are data files that sources can use for any purpose
+'''
 from .types import SourceSection, Suggestion
 from .source_google import get_suggestions as gs_google
 
